e503e269144514d5e161fdd817ea298bca60bbc1
BijanProgrammer <bijaneisapour@gmail.com>
2025-06-28 10:29:12 +0330

    [ADD] read leaderboard

--- src/controllers/history.controller.ts
+++ src/controllers/history.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { Sequelize } from "sequelize";
 
 import { DatabaseService } from "../services/database.service";
 
@@ -13,6 +14,24 @@ export class HistoryController {
     res.status(201).send({
       status: "success",
       message: "History created successfully.",
+    });
+  }
+
+  public async readLeaderboard(req: Request, res: Response): Promise<void> {
+    const records = await this.databaseService.History.findAll({
+      attributes: [
+        "username",
+        [Sequelize.fn("MAX", Sequelize.col("score")), "score"],
+      ],
+      group: "username",
+      order: [["score", "DESC"]],
+      limit: 5,
+    });
+
+    res.status(200).send({
+      status: "success",
+      message: "Leaderboard read successfully.",
+      data: records,
     });
   }
 }
--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -7,9 +7,15 @@ export function generatePublicRoutes(databaseService: DatabaseService): Router {
   const router = Router();
 
   const historyController = new HistoryController(databaseService);
+
   router.post(
     "/history/",
     historyController.createHistory.bind(historyController),
+  );
+
+  router.get(
+    "/leaderboard/",
+    historyController.readLeaderboard.bind(historyController),
   );
 
   return router;
