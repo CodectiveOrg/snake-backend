aab43b8ffad2b37e696bfb570e1e81287c7e9fca
Yasaman Mohammadi <118657398+yasibena@users.noreply.github.com>
2025-07-14 09:38:56 +0330

    Add swagger (#5)
    * add swwagger
    
    * add swagger

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ import { generatePublicRoutes } from "./routes/public.route";
 
 import { DatabaseService } from "./services/database.service";
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: "json" };
+
 import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
@@ -23,6 +26,8 @@ async function main(): Promise<void> {
   app.use(cors());
 
   app.use("/api/", generatePublicRoutes(databaseService));
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
