c3d21b9d46c914673c341013de4a932949cd2be7
Reza Mazaheri <163832802+mazaherireza@users.noreply.github.com>
2025-07-19 18:11:32 +0330

    Edit Profile Endpoint (#18)
    * [ADD] editProfile endpoint
    
    * [REMOVE] settings entity
    
    * [UPDATE] endpoint's route
    
    * [REFACTOR] editProfile
    
    * [FIX] swagger issues
    
    ---------
    
    Co-authored-by: BijanProgrammer <bijaneisapour@gmail.com>

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { DatabaseService } from "../services/database.service";
 import { User } from "../entities/user";
+import { z } from "zod";
+import { assignDefinedValues } from "../utils/object.utils";
 
 export class ProfileController {
   private profileRepo;
@@ -11,6 +13,7 @@ export class ProfileController {
     this.userRepo = databaseService.dataSource.getRepository(User);
 
     this.getProfile = this.getProfile.bind(this);
+    this.editProfile = this.editProfile.bind(this);
   }
 
   public async getProfile(_: Request, res: Response): Promise<void> {
@@ -34,4 +37,31 @@ export class ProfileController {
       data: record,
     });
   }
+
+  public async editProfile(req: Request, res: Response): Promise<void> {
+    const { username } = res.locals.user;
+
+    const body = EditProfileSchema.parse(req.body);
+
+    const user = await this.userRepo.findOne({ where: { username } });
+
+    if (!user) {
+      res.sendStatus(401);
+      return;
+    }
+
+    const updatedUser = assignDefinedValues(user, body);
+    await this.userRepo.save(updatedUser);
+
+    res.send({
+      status: "success",
+      message: "Profile updated successfully.",
+    });
+  }
 }
+
+const EditProfileSchema = z.object({
+  username: z.string().optional(),
+  email: z.string().email().optional(),
+  password: z.string().optional(),
+});
