fc1a7fbad4ff6fa24b732948b1185224b114090a
ali <moradkhani.mohammadali@gmail.com>
2025-07-19 10:27:42 +0330

    Create get user info by params

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { DatabaseService } from "../services/database.service";
+import { User } from "../entities/user";
+
+export class UserController {
+  private userRepo;
+
+  public constructor(databaseService: DatabaseService) {
+    this.userRepo = databaseService.dataSource.getRepository(User);
+
+    this.getUser = this.getUser.bind(this);
+  }
+
+  public async getUser(req: Request, res: Response): Promise<void> {
+    const userName = req.params.userName;
+
+    const user = await this.userRepo.findOne({
+      where: { username: userName },
+      select: { username: true, email: true },
+    });
+
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    res.status(200).send({
+      status: "success",
+      message: "User's info fetched successfully.",
+      data: user,
+    });
+  }
+}
--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -1,16 +1,19 @@
 import { Router } from "express";
 import { HistoryController } from "../controllers/history.controller";
+import { UserController } from "../controllers/user.controller";
 import { DatabaseService } from "../services/database.service";
 import { authMiddleware } from "../middlewares/auth.middleware";
 
 export function generatePublicRoutes(databaseService: DatabaseService): Router {
   const router = Router();
-  const controller = new HistoryController(databaseService);
+  const historyController = new HistoryController(databaseService);
+  const userController = new UserController(databaseService);
 
-  router.post("/history", authMiddleware, controller.createHistory);
-  router.get("/leaderboard", controller.getLeaderboard);
-  router.post("/rank", controller.getUserRank);
-  router.get("/high-score", authMiddleware, controller.getHighScore);
+  router.post("/history", authMiddleware, historyController.createHistory);
+  router.get("/leaderboard", historyController.getLeaderboard);
+  router.post("/rank", historyController.getUserRank);
+  router.get("/high-score", authMiddleware, historyController.getHighScore);
+  router.get("/:userName", userController.getUser);
 
   return router;
 }
