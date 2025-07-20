a2eebe21ec7152633c2ca4f98ff049fd79f2b321
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-20 11:25:20 +0330

    [REMOVE] unnecessary code

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,57 +1,6 @@
 import { Repository } from "typeorm";
 import { User } from "../entities/user";
 import { Response } from "express";
-import { z, ZodType } from "zod";
-
-type Options<TSchema extends ZodType> = {
-  res: Response;
-  parseUser?: boolean;
-  userRepo: Repository<User>;
-  parseBody?: boolean;
-  body: z.infer<TSchema>;
-  BodySchema: TSchema;
-};
-
-type Result<TSchema extends ZodType> =
-  | { error: true }
-  | {
-      user: User | null;
-      parsedBody: z.infer<TSchema> | null;
-    };
-
-export async function parseRequest<TSchema extends ZodType>({
-  res,
-  parseUser = true,
-  userRepo,
-  parseBody = false,
-  body,
-  BodySchema,
-}: Options<TSchema>): Promise<Result<TSchema>> {
-  let user: User | null = null;
-  let parsedBody: z.infer<TSchema> | null = null;
-
-  if (parseUser) {
-    const { username } = res.locals.user;
-
-    user = await userRepo.findOne({ where: { username } });
-
-    if (!user) {
-      res.status(401).send({
-        statusCode: 401,
-        message: "User not found.",
-        error: "Unauthorized",
-      });
-
-      return { error: true };
-    }
-  }
-
-  if (parseBody) {
-    parsedBody = BodySchema.parse(body);
-  }
-
-  return { user, parsedBody };
-}
 
 export async function fetchUserFromToken(
   res: Response,
