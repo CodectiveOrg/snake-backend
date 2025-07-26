d765ef27030c953d81692b2144948252966610b0
SinaMAmini <smobasheramini@gmail.com>
2025-07-26 13:17:42 +0330

    [ADD] error message to min and max functions

--- src/validation/schemas.ts
+++ src/validation/schemas.ts
@@ -1,13 +1,16 @@
 import { z } from "zod";
 
-export const UsernameSchema = z.string().min(3).max(16);
+export const UsernameSchema = z
+  .string()
+  .min(3, "Username must be at least 6 characters long")
+  .max(16, "Username must be a maximum of 16 characters long");
 
 export const EmailSchema = z.string().email();
 
 export const PasswordSchema = z
   .string()
-  .min(6)
-  .max(16)
+  .min(6, "Password must be at least 6 characters long")
+  .max(16, "Password must be a maximum of 16 characters long")
   .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
   .regex(/[a-z]/, "Password must contain at least one lowercase letter")
   .regex(/[0-9]/, "Password must contain at least one number");
