88c34343a125526275fed77d546b3edf6f6f2e8e
BijanProgrammer <bijaneisapour@gmail.com>
2025-07-27 08:52:14 +0330

    [UPDATE] global error handler response

--- src/handlers/global-error.handler.ts
+++ src/handlers/global-error.handler.ts
@@ -2,30 +2,32 @@ import { NextFunction, Request, Response } from "express";
 
 import { z } from "zod";
 
+import { ResponseDto } from "@/dto/response.dto";
+
 export async function globalErrorHandler(
   err: unknown,
   _req: Request,
-  res: Response,
+  res: Response<ResponseDto>,
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   _next: NextFunction,
 ): Promise<void> {
   if (err instanceof z.ZodError) {
     res.status(400).send({
-      status: "error",
-      message: "Invalid input",
-      details: err.issues,
+      statusCode: 400,
+      message: err.errors[0].message,
+      error: "Bad Request",
     });
   } else if (err instanceof Error) {
     res.status(500).send({
-      status: "error",
-      message: "Unexpected error",
-      details: err.message,
+      statusCode: 500,
+      message: err.message,
+      error: "Internal Server Error",
     });
   } else {
     res.status(500).send({
-      status: "error",
-      message: "Unexpected error",
-      details: "Internal server error",
+      statusCode: 500,
+      message: "Internal server error",
+      error: "Internal Server Error",
     });
   }
 }
